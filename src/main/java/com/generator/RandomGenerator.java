ea9cbf19044cb873e22a09726f792caf274df69a
tevfik gurkan <tevfikgurkan@tevfiks-MacBook-Pro.local>
2018-11-30 14:11:43 -0500

    Initial commit

---
+++ src/main/java/com/generator/RandomGenerator.java
@@ -0,0 +1,14 @@
+package com.generator;
+import java.util.*;
+//Class for normally distributed random variables
+public class RandomGenerator {
+	public List<Double> generate(){
+		List<Double> rnd = new ArrayList<>();
+		Random rand = new Random();
+		//Arraylist should hold Integer.MAX_VALUE doubles
+		for(int i = 0; i < 100000; i++) {
+			rnd.add(rand.nextGaussian());
+		}
+		return rnd;
+	}
+}
